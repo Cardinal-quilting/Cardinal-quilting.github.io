ac42efa6190c0611d87a9d548a078b76f5747203
Andy Davis <davisad@alum.mit.edu>
2022-04-27 20:19:32 -0400

    added javascript for header

---
+++ header.js
@@ -0,0 +1,16 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <link rel="stylesheet" type="text/css" href="styles.css">
+
+  <div class="header">
+    <img src="figures/CardinalQuiltsLogo.png" align="left">
+    <img src="figures/CardinalQuiltsNameLogo.png" align="top">
+  </div>
+  <div class="buttons">
+  <a class="page-link" href="index.html"><button class="button">Home</button></a>
+  <a class="page-link" href="paper-piecing.html"><button class="button">Paper piecing</button></a>
+  </div>
+`;
+
+document.body.prepend(template.content);
